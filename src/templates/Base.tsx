5f8fa605690488f42aa56674d54f2b210b556569
e-swissolar <vasileios.panagiotidis@gmail>
2022-02-11 16:01:59 +0100

    firstcommit

---
+++ src/templates/Base.tsx
@@ -0,0 +1,21 @@
+import { Meta } from '../layout/Meta';
+import { AppConfig } from '../utils/AppConfig';
+//import { Banner } from './Banner';
+import { Footer } from './Footer';
+import { Hero } from './Hero';
+import { VerticalFeatures } from './VerticalFeatures';
+
+const Base = () => (
+  //<div className="antialiased text-gray-600">
+  <div className="antialiased text-refi_blue_200">
+    <Meta title={AppConfig.title} description={AppConfig.description} />
+    <Hero />
+    <VerticalFeatures />
+    
+    <Footer />
+  </div>
+);
+
+export { Base };
+
+//<Banner />
---
+++ src/utils/AppConfig.ts
@@ -0,0 +1,6 @@
+export const AppConfig = {
+  site_name: 'ReFi',
+  title: 'Nextjs ReFi Starter',
+  description: 'Starter code for your Nextjs Boilerplate with Tailwind CSS',
+  locale: 'en',
+};
